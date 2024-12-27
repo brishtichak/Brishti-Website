aa9ea2e79483579fffb93d4b04aa2507d778df53
brishtichak <brishtic03@gmail.com>
2024-12-27 19:27:35 +0530

    fixed resume link again

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -10,7 +10,7 @@ const RPage = () => {
         <NavBar/>
         <h2>My Full Resume</h2>
         
-        <iframe src="https://drive.google.com/file/d/1Ws9HJPr_sMlpQxESks0FHYYUl0Vcaxoo/view?usp=sharinghttps://drive.google.com/file/d/1Ws9HJPr_sMlpQxESks0FHYYUl0Vcaxoo/preview" width="750" height="600" allow="autoplay"></iframe>
+        <iframe src="https://drive.google.com/file/d/1Ws9HJPr_sMlpQxESks0FHYYUl0Vcaxoo/preview" width="750" height="600" allow="autoplay"></iframe>
 
         <Footer></Footer>
         
